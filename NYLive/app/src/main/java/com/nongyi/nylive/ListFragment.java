f33fd59be5c6c9ee59a5de4e7e4b6fa920c55394
jaron.ho <892134825@qq.com>
2017-04-10 07:45:34 +0800

    修改NYLive

--- NYLive/app/src/main/java/com/nongyi/nylive/ListFragment.java
+++ NYLive/app/src/main/java/com/nongyi/nylive/ListFragment.java
@@ -3,12 +3,13 @@ package com.nongyi.nylive;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class ListFragment extends Fragment {
+    private RecyclerView mRecylerView = null;
 
     public ListFragment() {
     }
