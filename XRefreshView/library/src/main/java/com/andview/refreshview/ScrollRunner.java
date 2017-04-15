6ee5f772a7192fc93994b24082b7daf7c6e7aee4
jaron.ho <892134825@qq.com>
2017-04-16 00:18:12 +0800

    删除工程XRefreshView

--- XRefreshView/library/src/main/java/com/andview/refreshview/ScrollRunner.java
+++
@@ -1,9 +0,0 @@
-package com.andview.refreshview;
-
-/**
- * Created by huxq17 on 2016/12/15.
- */
-
-public abstract class ScrollRunner implements Runnable {
-    public boolean isStopLoadMore = false;
-}
