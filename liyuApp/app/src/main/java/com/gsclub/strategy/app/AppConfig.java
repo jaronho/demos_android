ff6399d2d32da47aca07a19b72f14715a5e25a59
jaron.ho <892134825@qq.com>
2018-11-02 01:00:23 +0800

    修改配置

--- liyuApp/app/src/main/java/com/gsclub/strategy/app/AppConfig.java
+++ liyuApp/app/src/main/java/com/gsclub/strategy/app/AppConfig.java
@@ -18,7 +18,7 @@ public interface AppConfig {
     String WX_APP_KEY = BuildConfig.ENV_DEBUG ? "wx4616a360fb2101a3" : "wxc55dfe7c6bf4e031";// 微信
     String WX_APP_SECRET = BuildConfig.ENV_DEBUG ? "e87279b620f391ded9480ab93e32cf41" : "896db2ef47f76ed0b0dd23c8d8cd9dbc";// 微信
     boolean LLENVIROMENT = false;//连连支付环境     true为测试环境，不推荐使用,false为正式环境
-    String UMENG_APP_KEY = BuildConfig.ENV_DEBUG ? "5bad8992f1f5563f04000190" : "5bad85d5f1f556470400006d";//测试环境鲸鱼选股测试账号，正式环境鲸鱼选股正式环境
+    String UMENG_APP_KEY = BuildConfig.ENV_DEBUG ? "5bdaf9eaf1f556fbc9000121" : "5bad85d5f1f556470400006d";//测试环境鲸鱼选股测试账号，正式环境鲸鱼选股正式环境
 
     /**
      * androidManifest 中配置的authorities
